f3c1b60f2726419dc556be57aae4a550e5847aed
Effie Mincer <effie.mincer@gmail.com>
2023-12-28 15:01:17 +0200

    Initial code + Welcome functions,

---
+++ Stage0/Program1982.cs
@@ -0,0 +1,21 @@
+﻿namespace Stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome1982();
+            Welcome5912(); 
+            Console.ReadKey();
+        }
+
+        static partial void Welcome5912();
+
+        private static void Welcome1982()
+        {
+            Console.Write("Enter your name: ");
+            String name = Console.ReadLine();
+            Console.WriteLine($"{name}, welcome to my first console application");
+        }
+    }
+}
