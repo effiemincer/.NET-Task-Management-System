2de3537fe0ee2926699bf92a3865799f870343c8
Effie Mincer <effie.mincer@gmail.com>
2024-01-08 00:37:25 +0200

    Dependency.cs

--- DalFacade/DO/Engineer.cs
+++ DalFacade/DO/Engineer.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace DO;
 
-
-//<param name="Id">A unique identifier (created automatically)</param>
-//<param name="Full name"></param>
-//<param name="Email address"></param>
-//<param name="Experience level"></param>
-//<param name="Cost per hour"></param>
+/// <summary> 
+/// Engineer 
+///</summary>
+/// <param name="Id">A unique identifier (created automatically)</param>
+/// <param name="Full name"></param>
+/// <param name="Email address"></param>
+/// <param name="Experience level"></param>
+/// <param name="Cost per hour"></param>
 public record Engineer
 (
     int ID,
