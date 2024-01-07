75d48da044327b2ad9af12c495a606790becd1c5
Effie Mincer <effie.mincer@gmail.com>
2024-01-07 23:54:22 +0200

    made changes

---
+++ DalFacade/DO/Engineer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DO;
+
+
+/*
+Engineer:
+ID - A unique identifier
+Full name
+Email address
+Experience level
+Cost per hour
+ */
+public record Engineer
+(
+    int ID,
+    string FullName,
+    string EmailAddress,
+    string? ExperienceLevel = null,
+    double? CostPerHour = null
+)
+{
+    public Engineer() : this (0, "", ""){ } //empty ctor for stage 3
+
+
+}
+
---
+++ DalFacade/DO/Task.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace DO;
+
+
+//<param name = "Id" > unique ID(created automatically)</param>
+
+//<param name = "Nickname"></param>
+//<param name = "Description"></param>
+//<param name = "Is it a milestone?">(T/F)</param>
+//<param name = "Date created"></param>
+//<param name = "Projected start Date"></param>
+//<param name = "Actual start Date"></param>
+//<param name = "Duration"></param>
+//<param name = "Deadline"></param>
+//<param name = "Actual end Date"></param>
+//<param name = "Deliverable"></param>
+//<param name = "Notes"></param>
+//<param name = "Assigned engineer ID"></param>
+//<param name = "Degree of difficulty"></param>
+public record Task
+(
+    int ID
+)
+{
+
+}
