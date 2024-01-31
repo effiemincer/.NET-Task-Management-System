8d0c860b9fc06203a161bfb79bc912a324e19b0f
rseltzer123 <rachamimseltzer@gmail.com>
2024-01-31 19:35:13 +0200

    more BO

---
+++ BL/BO/Engineer.cs
@@ -0,0 +1,18 @@
+﻿
+
+using static DO.Enums;
+
+namespace BO;
+
+public class Engineer
+{
+    public int Id { get; init; }
+    public string FullName { get; init; }
+    public string EmailAddress { get; init; }
+    //enum
+    //public Enums.EngineerExperience? ExperienceLevel {  get; set; }
+    public double CostPerHour {  get; init; }
+    public BO.TaskInEngineer? Task {  get; set; }
+    public bool Inactive {  get; }
+        
+}
---
+++ BL/BO/Task.cs
@@ -0,0 +1,29 @@
+﻿
+
+using DO;
+
+namespace BO;
+
+public class Task
+{
+    public int Id { get; init; }
+    public string Nickname { get; init; }
+    public DateTime DateCreated { get; init; }
+    public string Description { get; init; }
+    public BO.Status Status { get; init; }  
+    public List<BO.TaskInList>? Dependencies { get; set; }
+    public TimeSpan? Duration { get; set; }
+    public DateTime? Deadline { get; set; }
+    public DateTime? ScheduledStartDate { get; set; }
+    public DateTime? ProjectedStartDate {  get; set; }
+    public Enums.EngineerExperience? DegreeOfDifficulty {  get; set; }
+    public int? AssignedEngineerId {  get; set; }
+    public DateTime? ActualEndDate { get; set; }
+    public bool IsMilestone {  get; set; }
+    public DateTime? ActualStartDate {  get; set; }
+    public string? Deliverable {  get; set; }
+    public BO.EngineerInTask? Engineer { get; set; }
+    public BO.EngineerExperience? Complexity { get; set; }
+    public string? Notes {  get; set; }
+    public bool Inactive { get; }
+}
