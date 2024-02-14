8b0a2a7953fd1af77ae1c3fb295441417bf29a33
Effie Mincer <effie.mincer@gmail.com>
2024-02-14 16:34:31 +0200

    adding milestone and task

--- BL/BlApi/IMillestone.cs
+++ BL/BlApi/IMillestone.cs
@@ -4,4 +4,9 @@ namespace BlApi;
 
 public interface IMillestone
 {
+    public int Create(BO.Milestone item);
+    public BO.Milestone Read(int id);
+    public IEnumerable<BO.MilestoneInList> ReadAll();
+    public void Update(BO.Milestone item);
+    public void Delete(int id);
 }
