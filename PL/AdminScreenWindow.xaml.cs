b05044eda04524a3ec8243ad410ee7bba4907512
rseltzer123 <rachamimseltzer@gmail.com>
2024-02-21 18:27:22 +0200

    Pl started

---
+++ PL/AdminScreenWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using BO;
+using Engineer;
+using Milestone;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Task;
+
+namespace PL
+{
+    /// <summary>
+    /// Interaction logic for AdminScreenWindow.xaml
+    /// </summary>
+    public partial class AdminScreenWindow : Window
+    {
+        public AdminScreenWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Manage_Tasks_Click(object sender, RoutedEventArgs e)
+        {
+            // send IsAdmin is true
+            new TaskListWindow(true).Show();
+        }
+
+        private void Manage_Engineers_Click(object sender, RoutedEventArgs e)
+        {
+            new EngineerListWindow().Show();
+        }
+
+        private void Manage_Milestones_Click(object sender, RoutedEventArgs e)
+        {
+            new MilestoneListWindow().Show();
+        }
+    }
+}
---
+++ PL/EngineerScreenWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Task;
+
+namespace PL
+{
+    /// <summary>
+    /// Interaction logic for EngineerScreenWindow.xaml
+    /// </summary>
+    public partial class EngineerScreenWindow : Window
+    {
+        public EngineerScreenWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Tasks_Button_Click(object sender, RoutedEventArgs e)
+        {
+            // IsAdmin is false
+            new TaskListWindow(false).Show();
+        }
+
+        private void Current_Task_Button_Click(object sender, RoutedEventArgs e)
+        {
+            new TaskWindow().Show();
+        }
+    }
+}
