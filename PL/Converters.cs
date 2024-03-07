925dd6ea60726515dd5a12a72fb8306b397ea81f
Effie Mincer <effie.mincer@gmail.com>
2024-03-07 22:08:49 +0200

    converter

--- PL/Converters.cs
+++ PL/Converters.cs
@@ -8,13 +8,15 @@ using System.Windows.Data;
 
 namespace PL;
 
-class ConvertIdToContent : IValueConverter 
-{ 
-    public object Convert(object value, Type targetType, object parameter, CultureInfo culture) 
-    { 
-        return (int)value == 0 ? "Add" : "Update"; 
+
+class ConvertIdToContent : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return (int)value == 0 ? "Add" : "Update";
     }
-    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) 
-    { throw new NotImplementedException(); 
-    } 
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
 }
