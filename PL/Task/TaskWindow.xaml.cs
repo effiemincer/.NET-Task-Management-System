1cb59aec83bf90c6f0b81486a3c6205a67c46c92
rseltzer123 <rachamimseltzer@gmail.com>
2024-03-14 00:46:20 +0200

    fixed button

--- PL/Task/TaskWindow.xaml.cs
+++ PL/Task/TaskWindow.xaml.cs
@@ -28,65 +28,91 @@ namespace Task
         //        new PropertyMetadata(null)
         //    );
 
+        // Dependency property to bind a Task object
         public static readonly DependencyProperty TaskProperty =
             DependencyProperty.Register("CurrentTask", typeof(BO.Task), typeof(TaskWindow), new PropertyMetadata(null));
 
+        // Property to get or set the current task
         public BO.Task CurrentTask
         {
             get { return (BO.Task)GetValue(TaskProperty); }
             set { SetValue(TaskProperty, value); }
         }
 
-        private bool isAdd;
+        private bool isAdd; // Flag indicating if it's adding a new task
 
-        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+        private bool isDeliverableFirstRun; // Flag indicating if the deliverable property has been set for the first time
 
+        static readonly BlApi.IBl s_bl = BlApi.Factory.Get(); // Reference to business logic instance
+
+        // Constructor
         public TaskWindow(BO.Task task_, bool isAdd_ = false)
         {
             InitializeComponent();
-            CurrentTask = task_;
-            isAdd = isAdd_;
+            CurrentTask = task_; // Initialize current task
+            isAdd = isAdd_; // Initialize flag for adding a task
+            isDeliverableFirstRun = task_.Deliverable; // Initialize flag for deliverable property
         }
 
-        //public IEnumerable<BO.TaskInList> TaskList
-        //{
-        //    get { return (IEnumerable<BO.TaskInList>)GetValue(TaskListProperty); }
-        //    set { SetValue(TaskListProperty, value); }
-        //}
-
-        
+        // TextChanged event handler for TextBox (currently not used)
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             return;
         }
 
+        // Click event handler for Add/Update button
         private void btnAddUpdate_Click(object sender, RoutedEventArgs e)
         {
+            // Add or update task based on isAdd flag
             if (isAdd)
             {
                 s_bl?.Task.Create(CurrentTask);
-                
             }
             else
             {
                 s_bl?.Task.Update(CurrentTask);
             }
-            Close();
+            Close(); // Close the window
         }
 
+        // SelectionChanged event handler for Task Status ComboBox (currently not used)
         private void cbTaskStatus_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            // Update TaskList based on selected task status
             //TaskList = (TaskStatus == BO.Enums.Status.None) ? s_bl?.Task.ReadAll()! : s_bl?.Task.ReadAll(task => task.Status == TaskStatus)!;
             return;
         }
 
-        //private void RadioButton_Click(object sender, RoutedEventArgs e)
-        //{
-        //    RadioButton radioButton = sender as RadioButton;
-        //    if (radioButton != null)
-        //    {
-        //        radioButton.IsChecked = !radioButton.IsChecked;
-        //    }
-        //}
+        // Flag to indicate if RadioButton was just checked
+        private bool JustChecked;
+
+        // Checked event handler for RadioButton
+        private void RB_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton s = sender as RadioButton;
+            // Action on Check...
+
+            JustChecked = true;
+            if (isDeliverableFirstRun)
+            {
+                JustChecked = false;
+                isDeliverableFirstRun = false;
+            }
+        }
+
+        // Clicked event handler for RadioButton
+        private void RB_Clicked(object sender, RoutedEventArgs e)
+        {
+            if (JustChecked)
+            {
+                JustChecked = false;
+                e.Handled = true;
+                return;
+            }
+            RadioButton s = sender as RadioButton;
+            if ((bool)s.IsChecked)
+                s.IsChecked = false;
+
+        }
     }
 }
