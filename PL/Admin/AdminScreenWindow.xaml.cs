bc059ce0bc8ca7406c8e3d389a34e8ce2ee63352
Effie Mincer <effie.mincer@gmail.com>
2024-03-23 22:00:16 +0200

    minor fix

--- PL/Admin/AdminScreenWindow.xaml.cs
+++ PL/Admin/AdminScreenWindow.xaml.cs
@@ -3,228 +3,207 @@ using BO; // Importing the BO namespace for access to its functionalities.
 using Engineer; // Importing the Engineer namespace for access to its functionalities.
 using Milestone; // Importing the Milestone namespace for access to its functionalities.
 using PL.Admin;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+
 using Task; // Importing the Task namespace for access to its functionalities.
 
-namespace PL; // Declaring the namespace for the PL (Presentation Layer) class.
-
-/// <summary>
-/// Interaction logic for AdminScreenWindow.xaml
-/// </summary>
-public partial class AdminScreenWindow : Window
+namespace PL // Declaring the namespace for the PL (Presentation Layer) class.
 {
-    // Constructor for AdminScreenWindow class.
-    
-    static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
-
-    private bool? ScheduleCreated = s_bl.Config.GetIsScheduleGenerated();
-
-    // Dependency property for binding the current engineer.
-    public static readonly DependencyProperty TimeProperty =
-        DependencyProperty.Register("CurrentTime", typeof(DateTime), typeof(AdminScreenWindow), new PropertyMetadata(null));
-
-    // Property to get or set the current engineer.
-    public DateTime CurrentTime
+    /// <summary>
+    /// Interaction logic for AdminScreenWindow.xaml
+    /// </summary>
+    public partial class AdminScreenWindow : Window
     {
-        get { return (DateTime)GetValue(TimeProperty); }
-        set { SetValue(TimeProperty, s_bl.Clock); }
-    }
+        // Constructor for AdminScreenWindow class.
+        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
 
-    public AdminScreenWindow()
-    {
-        InitializeComponent(); // Initializes the window components.
-        CurrentTime = s_bl.Clock; // Sets the current time.    
-        ScheduleCreated = s_bl.Config.GetIsScheduleGenerated();
-        if (ScheduleCreated is null) {
-            _milestones.IsEnabled = false;
-            _gantt.IsEnabled = false;
-            _generate.IsEnabled = true;
-            _terminate.IsEnabled = false;
+        private bool? ScheduleCreated = s_bl.Config.GetIsScheduleGenerated();
 
-        }
-        else if (!(bool)ScheduleCreated)
-        {
-            _generate.IsEnabled = true; 
-            _terminate.IsEnabled = false;
-        }
-        else
-        {
-            _generate.IsEnabled= false;
-            _terminate.IsEnabled= true;
-        }
-    }
+        // Dependency property for binding the current engineer.
+        public static readonly DependencyProperty TimeProperty =
+            DependencyProperty.Register("CurrentTime", typeof(DateTime), typeof(AdminScreenWindow), new PropertyMetadata(null));
 
-    // Event handler for "Manage Tasks" button click.
-    private void Manage_Tasks_Click(object sender, RoutedEventArgs e)
-    {
-        // Opens the TaskListWindow with isAdmin set to true, indicating admin access.
-        new TaskListWindow(true).Show();
-    }
-
-    // Event handler for "Manage Engineers" button click.
-    private void Manage_Engineers_Click(object sender, RoutedEventArgs e)
-    {
-        // Opens the EngineerListWindow.
-        new EngineerListWindow().Show();
-    }
-
-    // Event handler for "Manage Milestones" button click.
-    private void Manage_Milestones_Click(object sender, RoutedEventArgs e)
-    {
-        if (ScheduleCreated is null)
+        // Property to get or set the current engineer.
+        public DateTime CurrentTime
         {
-            MessageBox.Show("You must initialize data.", "NoIsScheduleGeneratedInXML", MessageBoxButton.OK, MessageBoxImage.Error);
-        }
-        else if ((bool)ScheduleCreated)
-        {
-            // Opens the MilestoneListWindow.
-            new MilestoneListWindow().Show();
-        }
-        else
-        {
-            MessageBox.Show("You must generate a schedule first in order to see milestones.", "NoScheduleGenerated", MessageBoxButton.OK, MessageBoxImage.Error);
-        }
-    }
-
-    private void Gantt_Chart_Click(object sender, RoutedEventArgs e)
-    {
-        if (ScheduleCreated is null)
-        {
-            MessageBox.Show("You must initialize data.", "NoIsScheduleGeneratedInXML", MessageBoxButton.OK, MessageBoxImage.Error);
-        }
-        else if ((bool)ScheduleCreated)
-        {
-            // Opens the Gantt Chart Window.
-            new GanttChartWindow().Show();
-        }
-        else
-        {
-            MessageBox.Show("You must generate a schedule first in order to see the gantt chart view.", "NoScheduleGenerated", MessageBoxButton.OK, MessageBoxImage.Error);
-        }
-        
-    }
-
-    private void Add_Engineer_Click(object sender, RoutedEventArgs e)
-    {
-        new EngineerWindow(new BO.Engineer(), true).ShowDialog();
-    }
-    
-    private void Generate_Schedule_Terminate_Project_Click(object sender, RoutedEventArgs e)
-    {
-
-        //Button button = sender as Button;
-        IEnumerable<BO.TaskInList?> taskInLists = s_bl.Task.ReadAll();
-        if (taskInLists is null || s_bl.Config.GetProjectStartDate() is null || s_bl.Config.GetProjectEndDate() is null)
-        {
-            MessageBox.Show("You must initialize data.", "NoIsScheduleGeneratedInXML", MessageBoxButton.OK, MessageBoxImage.Error);
+            get { return (DateTime)GetValue(TimeProperty); }
+            set { SetValue(TimeProperty, s_bl.Clock); }
         }
 
-        //generate schedule
-        else if (ScheduleCreated is null || !(bool)ScheduleCreated )
-        {   
-            //generates schedule and locks certain modifcations 
-            try
+        public AdminScreenWindow()
+        {
+            InitializeComponent(); // Initializes the window components.
+            CurrentTime = s_bl.Clock; // Sets the current time.    
+            ScheduleCreated = s_bl.Config.GetIsScheduleGenerated();
+            if (ScheduleCreated is null)
             {
-                //need to put in actual dates here probably prompt the user or something 
-                s_bl.Milestone.CreateSchedule(DateTime.MinValue, DateTime.MaxValue);
-                ScheduleCreated = true;
-                s_bl.Config.SetIsScheduleGenerated(true);
+                _milestones.IsEnabled = false;
+                _gantt.IsEnabled = false;
+                _generate.IsEnabled = true;
+                _terminate.IsEnabled = false;
+            }
+            else if (!(bool)ScheduleCreated)
+            {
+                _generate.IsEnabled = true;
+                _terminate.IsEnabled = false;
+            }
+            else
+            {
                 _generate.IsEnabled = false;
                 _terminate.IsEnabled = true;
-                _gantt.IsEnabled = true;
-                _milestones.IsEnabled = true;
-
-                MessageBox.Show("Schedule has been generated.", "GenerationSuccess", MessageBoxButton.OK, MessageBoxImage.Information);
             }
-            catch (Exception ex)
+        }
+
+        // Event handler for "Manage Tasks" button click.
+        private void Manage_Tasks_Click(object sender, RoutedEventArgs e)
+        {
+            // Opens the TaskListWindow with isAdmin set to true, indicating admin access.
+            new TaskListWindow(true).Show();
+        }
+
+        // Event handler for "Manage Engineers" button click.
+        private void Manage_Engineers_Click(object sender, RoutedEventArgs e)
+        {
+            // Opens the EngineerListWindow.
+            new EngineerListWindow().Show();
+        }
+
+        // Event handler for "Manage Milestones" button click.
+        private void Manage_Milestones_Click(object sender, RoutedEventArgs e)
+        {
+            if (ScheduleCreated is null)
             {
-                MessageBox.Show(ex.Message, "Exception", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("You must initialize data.", "NoIsScheduleGeneratedInXML", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else if ((bool)ScheduleCreated)
+            {
+                // Opens the MilestoneListWindow.
+                new MilestoneListWindow().Show();
+            }
+            else
+            {
+                MessageBox.Show("You must generate a schedule first in order to see milestones.", "NoScheduleGenerated", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
-        //terminate schedule
-        else
+
+        private void Gantt_Chart_Click(object sender, RoutedEventArgs e)
         {
-            //terminate program was pressed
-            //s_bl.Config.SetProjectStartDate(null);
-            //s_bl.Config.SetProjectEndDate();
-            ScheduleCreated = false;
-            s_bl.Config.SetIsScheduleGenerated(false);
-            _generate.IsEnabled = true;
-            _terminate.IsEnabled = false;
-            _gantt.IsEnabled = false;
-            _milestones.IsEnabled = false;
+            if (ScheduleCreated is null)
+            {
+                MessageBox.Show("You must initialize data.", "NoIsScheduleGeneratedInXML", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else if ((bool)ScheduleCreated)
+            {
+                // Opens the Gantt Chart Window.
+                new GanttChartWindow().Show();
+            }
+            else
+            {
+                MessageBox.Show("You must generate a schedule first in order to see the gantt chart view.", "NoScheduleGenerated", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
 
-            s_bl.Milestone.Reset();
-
-            MessageBox.Show("Schedule has been terminated.", "TerminationSuccess", MessageBoxButton.OK, MessageBoxImage.Information);
         }
-        
 
-    }
-
-    private void Set_Project_Dates_Click(object sender, RoutedEventArgs e)
-    {
-        MessageBoxResult res = MessageBox.Show("By changing the Project Start and End Dates you will reset all data. Are you sure you want to reset all the data?", "ResetConfirmation", MessageBoxButton.YesNo, MessageBoxImage.Warning);
-        if (res == MessageBoxResult.Yes)
+        private void Add_Engineer_Click(object sender, RoutedEventArgs e)
         {
-            _milestones.IsEnabled = false;
-            _gantt.IsEnabled = false;
-            _generate.IsEnabled = true;
-            _terminate.IsEnabled = false;
-            s_bl.Config.Reset();
-            s_bl.Milestone.Reset();
-            //dataInitialized = false;
-            MessageBox.Show("Data was reset. You may now change project start and end dates.", "ResetSuccessful", MessageBoxButton.OK, MessageBoxImage.Information);
-            new ProjectDatesWindow().ShowDialog();
+            new EngineerWindow(new BO.Engineer(), true).ShowDialog();
         }
-        
-    }
 
-    private void Travel_Forwards_Day_Click(object sender, RoutedEventArgs e)
-    {
-        s_bl.travelForwardsDay();
-        CurrentTime = s_bl.Clock;
-    }
+        private void Generate_Schedule_Terminate_Project_Click(object sender, RoutedEventArgs e)
+        {
+            IEnumerable<BO.TaskInList?> taskInLists = s_bl.Task.ReadAll();
+            if (taskInLists is null || s_bl.Config.GetProjectStartDate() is null || s_bl.Config.GetProjectEndDate() is null)
+            {
+                MessageBox.Show("You must initialize data.", "NoIsScheduleGeneratedInXML", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else if (ScheduleCreated is null || !(bool)ScheduleCreated)
+            {
+                // Generates schedule and locks certain modifications.
+                try
+                {
+                    s_bl.Milestone.CreateSchedule((DateTime)s_bl.Config.GetProjectStartDate()!, (DateTime)s_bl.Config.GetProjectEndDate()!);
+                    ScheduleCreated = true;
+                    s_bl.Config.SetIsScheduleGenerated(true);
+                    _generate.IsEnabled = false;
+                    _terminate.IsEnabled = true;
+                    _gantt.IsEnabled = true;
+                    _milestones.IsEnabled = true;
 
-    private void Travel_Forwards_Hour_Click(object sender, RoutedEventArgs e)
-    {
-        s_bl.travelForwardsHour();
-        CurrentTime = s_bl.Clock;
-    }
+                    MessageBox.Show("Schedule has been generated.", "GenerationSuccess", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Exception", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                // Terminate program was pressed.
+                ScheduleCreated = false;
+                s_bl.Config.SetIsScheduleGenerated(false);
+                _generate.IsEnabled = true;
+                _terminate.IsEnabled = false;
+                _gantt.IsEnabled = false;
+                _milestones.IsEnabled = false;
 
-    private void Travel_Backwards_Day_Click(object sender, RoutedEventArgs e)
-    {
-        s_bl.travelBackwardDay();
-        CurrentTime = s_bl.Clock;
-    }
+                s_bl.Milestone.Reset();
 
-    private void Travel_Backwards_Hour_Click(object sender, RoutedEventArgs e)
-    {
-        s_bl.travelBackwardHour();
-        CurrentTime = s_bl.Clock;
-    }
+                MessageBox.Show("Schedule has been terminated.", "TerminationSuccess", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
 
-    private void Reset_Clock_Click(object sender, RoutedEventArgs e)
-    {
-        s_bl.resetClock();
-        CurrentTime = s_bl.Clock;
-    }
+        private void Set_Project_Dates_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult res = MessageBox.Show("By changing the Project Start and End Dates you will reset all data. Are you sure you want to reset all the data?", "ResetConfirmation", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (res == MessageBoxResult.Yes)
+            {
+                _milestones.IsEnabled = false;
+                _gantt.IsEnabled = false;
+                _generate.IsEnabled = true;
+                _terminate.IsEnabled = false;
+                s_bl.Config.Reset();
+                s_bl.Milestone.Reset();
+                // DataInitialized = false;
+                MessageBox.Show("Data was reset. You may now change project start and end dates.", "ResetSuccessful", MessageBoxButton.OK, MessageBoxImage.Information);
+                new ProjectDatesWindow().ShowDialog();
+            }
 
-    private void Back_Click(object sender, RoutedEventArgs e)
-    {
-        new MainWindow().Show();
-        Close();
+        }
+
+        private void Travel_Forwards_Day_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.travelForwardsDay();
+            CurrentTime = s_bl.Clock;
+        }
+
+        private void Travel_Forwards_Hour_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.travelForwardsHour();
+            CurrentTime = s_bl.Clock;
+        }
+
+        private void Travel_Backwards_Day_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.travelBackwardDay();
+            CurrentTime = s_bl.Clock;
+        }
+
+        private void Travel_Backwards_Hour_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.travelBackwardHour();
+            CurrentTime = s_bl.Clock;
+        }
+
+        private void Reset_Clock_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.resetClock();
+            CurrentTime = s_bl.Clock;
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            new MainWindow().Show();
+            Close();
+        }
     }
 }
